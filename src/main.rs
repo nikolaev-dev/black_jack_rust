3f6ddbdd00f29f3bf1bf9e811caa37173567b7cb
Alexander Nikolaev <ni-ko-la@mail.ru>
2019-01-05 00:35:34 +0300

    create player_cards_to_str(&mut self, cards: Vec<Card>)

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ struct Round {
     dealer_score: i32,
     deck:         Vec<Card>,
     player_cards: Vec<Card>,
+    player_cards_str: Vec<String>,
     dealer_cards: Vec<Card>
 }
 
@@ -31,6 +32,10 @@ impl Round {
 
         self.score();
     }
+
+    fn player_cards_to_str(&mut self, cards: Vec<Card>) {
+        self.player_cards_str = cards.into_iter().map(|c| c.name).collect();
+    }
 }
 
 
@@ -44,8 +49,17 @@ fn main() {
     println!("player_name:  {}", round.player_name);
     println!("player_score: {}", round.player_score);
     println!("dealer_score: {}", round.dealer_score);
+
+
+    let cards: Vec<Card> = vec![];
+    round.player_cards_to_str(cards);
+
+//    let player_cards = round.player_cards.clone();
+
     let pcards:Vec<String> = round.player_cards.into_iter().map(|c| c.name).collect();
     let dcards:Vec<String> = round.dealer_cards.into_iter().map(|c| c.name).collect();
+
+    println!("player cards: {:?}", round.player_cards_str );
     println!("player cards: {:?}", pcards );
     println!("dealer cards: {:?}", dcards );
 
@@ -54,11 +68,13 @@ fn main() {
 
 
 fn initial_round() -> Round {
+    let s_slice: Vec<String> = vec![];
     Round {
         player_name: String::new(),
         player_score: 0,
         dealer_score: 0,
         player_cards: vec![],
+        player_cards_str: s_slice,
         dealer_cards: vec![],
         deck: generate_cards()
     }
